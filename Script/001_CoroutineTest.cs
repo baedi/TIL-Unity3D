e4da8e373d27c1866ad66a439f99d70579f8c626
Daeil Bae <47047263+baedi@users.noreply.github.com>
2019-12-04 20:17:03 +0900

    Unity Study (2019-12-04)
    - GameObject 클래스에 대한 기본 이해
    - GameObject 객체가 private인 상태에서 특정 오브젝트를 가져오는 방법에 대한 이해
    - 코루틴 함수의 기본 동작 이해 (스레드 대신 사용하면 좋음)
    - 오브젝트 기본 이동 방법에 대한 이해

---
+++ Script/001_CoroutineTest.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoroutineTest : MonoBehaviour {
+
+    // 변수                     
+    private GameObject cube;
+    public float moveSpeed = 0.3f;
+    public bool moveMode = false;
+
+    // 메인 함수                
+    void Start() {
+        cube = GameObject.Find("TestCube");     // TestCube라는 이름을 찾아 cube 객체에 삽입한다. 
+        StartCoroutine(TestCoroutine());        // 코루틴 함수를 실행한다.                        
+    }
+
+    // 업데이트 함수            
+    void Update() {
+        moveControl(moveMode);
+    }
+
+    // 코루틴 함수              
+    private IEnumerator TestCoroutine() {
+
+        // 무한 루프    
+        while (true) {
+            yield return new WaitForSeconds(1); // 1초 대기        
+            moveMode = !moveMode;               // 이동 방향 전환  
+        }
+    }
+
+    // 이동 메서드              
+    private void moveControl(bool isPlusX) {
+        if (isPlusX)    transform.Translate(moveSpeed, 0, 0);   //  x축 이동   
+        else            transform.Translate(-moveSpeed, 0, 0);  // -x축 이동   
+        
+    }
+}
