75463aee824787a743e17c025391fbe421913d67
Daeil Bae <47047263+baedi@users.noreply.github.com>
2019-12-05 12:00:32 +0900

    Unity Study (2019-12-05)
    - 키 코드와 키를 입력받는 방법에 대한 이해
    - 물체 충돌을 감지하는 관련 함수들에 대한 이해
    - 직접 컴포넌트를 이용하지 않고도 옵션을 변경할 수 있는 방법에 대한 이해
    - 태그를 이용한 충돌 함수 응용

---
+++ Script/002_Control.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Control : MonoBehaviour {
+
+    private new Rigidbody rigidbody;    // 중력 관련 컴포넌트   
+    private bool isJumpEnable;          // 점프 활성화 여부     
+    public float speed = 0.1f;          // 이동 속도            
+    public float jump = 0.2f;           // 점프 세기            
+
+    // Start is called before the first frame update
+    void Start() {
+
+        
+        rigidbody = GetComponent<Rigidbody>();  // 이 스크립트를 적용한 오브젝트의 Rigidbody 컴포넌트를 가져옴.   
+        rigidbody.freezeRotation = true;        // 물리적 충돌에 의한 기울기가 변경되지 않도록 함.                
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetKey(KeyCode.W)) transform.Translate(0, 0, speed);      // W키(전진)       
+        if (Input.GetKey(KeyCode.A)) transform.Translate(-speed, 0, 0);     // A키(좌측)       
+        if (Input.GetKey(KeyCode.S)) transform.Translate(0, 0, -speed);     // S키(후진)       
+        if (Input.GetKey(KeyCode.D)) transform.Translate(speed, 0, 0);      // D키(우측)       
+        if (Input.GetKey(KeyCode.Space) && !isJumpEnable){                  // SPACE키 (점프)  
+            rigidbody.AddForce(Vector3.up * jump, ForceMode.Impulse);           // 점프            
+            isJumpEnable = true;                                                // 점프 중인 경우이므로 true로 설정. 
+        }
+    }
+
+    // Ground 라는 태그를 가진 물체와 충돌 시 점프 상태가 아님을 취함.
+    private void OnCollisionEnter(Collision collision) {
+        if (collision.gameObject.CompareTag("Ground") || collision.gameObject.CompareTag("MoveGround"))
+            isJumpEnable = false;
+    }
+}
