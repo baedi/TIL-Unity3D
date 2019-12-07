b6112d3be35c72a061a3a7e25f502889058a1d65
Daeil Bae <47047263+baedi@users.noreply.github.com>
2019-12-07 22:33:26 +0900

    Unity Study (2019-12-07)
    - Material 클래스에 대한 기본 이해
    - 코루틴 함수를 이용한 이펙트 지속시간 활용 이해
    
    (수정 사항 :: 002_Control.cs)
    - 바닥에 닿은 상태에서 점프하지 않은 채 떨어질 경우 공중점프가 되지 않도록 코드를 수정함.

--- Script/004_Control-2.cs
+++ Script/004_Control-2.cs
@@ -6,6 +6,8 @@ public class Control : MonoBehaviour {
 
     private new Rigidbody rigidbody;    // 중력 관련 컴포넌트   
     private bool isJumpEnable;          // 점프 활성화 여부     
+    public Material matColor;           // 색깔 머터리얼        
+
     public float speed = 0.1f;          // 이동 속도            
     public float jump = 0.2f;           // 점프 세기            
 
@@ -15,6 +17,8 @@ public class Control : MonoBehaviour {
         
         rigidbody = GetComponent<Rigidbody>();  // 이 스크립트를 적용한 오브젝트의 Rigidbody 컴포넌트를 가져옴.   
         rigidbody.freezeRotation = true;        // 물리적 충돌에 의한 기울기가 변경되지 않도록 함.                
+
+        matColor.color = Color.white;
     }
 
     // Update is called once per frame
@@ -23,6 +27,8 @@ public class Control : MonoBehaviour {
         if (Input.GetKey(KeyCode.A)) transform.Translate(-speed, 0, 0);     // A키(좌측)       
         if (Input.GetKey(KeyCode.S)) transform.Translate(0, 0, -speed);     // S키(후진)       
         if (Input.GetKey(KeyCode.D)) transform.Translate(speed, 0, 0);      // D키(우측)       
+        if (Input.GetKeyDown(KeyCode.Q) && matColor.color != Color.red)     // Q키(색깔바꾸기) 
+            StartCoroutine(Change30SecColor());
         if (Input.GetKey(KeyCode.Space) && !isJumpEnable){                  // SPACE키 (점프)  
             rigidbody.AddForce(Vector3.up * jump, ForceMode.Impulse);           // 점프            
             isJumpEnable = true;                                                // 점프 중인 경우이므로 true로 설정. 
@@ -37,5 +43,24 @@ public class Control : MonoBehaviour {
 
     private void OnCollisionExit(Collision collision) {
         isJumpEnable = true;
+    }
+
+
+    // 오브젝트 색깔을 일정 시간 동안 변경시키는 코루틴 함수           
+    private IEnumerator Change30SecColor() {
+        Debug.Log("Coroutine is ON!");                  // 로그 띄우기   
+
+        // 오브젝트 색깔을 빨간색으로 변경      
+        matColor.color = Color.red;
+
+        // 5초간 지속                           
+        for (int count = 5; count > 0; count--) {
+            Debug.Log("Time : " + count);
+            yield return new WaitForSeconds(1);
+        }
+
+        Debug.Log("Effect timeout!");       // 로그 띄우기    
+        matColor.color = Color.white;       // 흰색으로 변경  
+        yield return null;                  // 코루틴 함수 끝 
     }
 }
