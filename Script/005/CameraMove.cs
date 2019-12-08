1284ed06f1e8818dfe452f904128cfc92eb71f92
Daeil Bae <47047263+baedi@users.noreply.github.com>
2019-12-08 22:38:22 +0900

    Unity Study (2019-12-08)
    - 기본 회전에 관한 이해
    - Vector3 사용에 관한 이해
    - 포지션(위치), 회전축 설정 방법에 관한 이해

---
+++ Script/005/CameraMove.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour
+{
+    // 변수       
+    private Transform controlTransform;     // 컨트롤 전용 오브젝트의 Transform   
+
+    // 시작       
+    void Start()
+    {
+        // ControlCube 라는 이름의 오브젝트의 transform을 가져와서 변수에 할당하고     
+        // 카메라는 해당 오브젝트의 자식이 됨.                                         
+        transform.parent = controlTransform = GameObject.Find("ControlCube").transform;
+
+        // 카메라 위치 설정                    
+        transform.SetPositionAndRotation(
+            new Vector3(controlTransform.position.x,            // 컨트롤 오브젝트의 X축 
+                        controlTransform.position.y + 0.5f,     // 컨트롤 오브젝트의 Y축 
+                        controlTransform.position.z),           // 컨트롤 오브젝트의 Z축 
+            new Quaternion()
+        );
+    }
+
+    // 루프       
+    void Update()
+    {
+        
+    }
+}
---
+++ Script/005/ControlRotate.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlRotate : MonoBehaviour
+{
+
+    public float rotateSpeed = 100.0f;    // 회전 속도            
+    private float xMouse;                 // 마우스 X축           
+
+    // 시작 함수    
+    void Start() { }
+
+    // 루프 함수    
+    void Update()
+    {
+        xMouse = Input.GetAxis("Mouse X");
+        Vector3 rotateSetting = Vector3.up * xMouse;
+
+        transform.Rotate(rotateSetting * rotateSpeed * Time.deltaTime);
+    }
+}
