28863c7248bca1292c7f11ad5d815229d62f5a0d
Daeil Bae <47047263+baedi@users.noreply.github.com>
2019-12-19 12:15:55 +0900

    Unity Study (2019-12-19)
    (AccControl.cs 변경사항)
    - 점프 관련 코드 추가
    - 움직임 관련 속도 조절
    - 계속 전진하고 있으면 속도가 점점 올라가도록 지연시간을 수식으로 조절함.

--- Script/010/AccControl.cs
+++ Script/010/AccControl.cs
@@ -5,13 +5,16 @@ using UnityEngine;
 public class AccControl : MonoBehaviour {
 
     // 변수            
-    private const int _MAX_VALUE = 10;
-    private const int _MIN_VALUE = 3;
+    private const int _MAX_VALUE = 50;          // 최대 
+    private const int _MIN_VALUE = 3;           // 최소 
     private new Rigidbody rigidbody;
 
-    private byte sp_ = (byte)_MIN_VALUE;    // 이동 속도 계산용    
-    private float vertical;
-    public float runSpeed = 0.1f;           // 이동 속도           
+    private int moveLevel = _MIN_VALUE;         // 이동 속도 계산용  (초기값 : _MIN_VALUE)  
+    private float vertical;                     // ?        
+    private bool isJumpEnable;                  // 점프 가능 여부                           
+
+    public float runSpeed = 0.1f;               // 이동 속도           
+    public float jumpPower = 5f;                // 점프력              
 
 
 
@@ -21,6 +24,7 @@ public class AccControl : MonoBehaviour {
 
         rigidbody = GetComponent<Rigidbody>();  // 이 스크립트를 적용한 오브젝트의 Rigidbody 컴포넌트를 가져옴.    
         rigidbody.freezeRotation = true;        // 캐릭터의 회전축이 기울어지는 문제를 방지하기 위함.              
+        isJumpEnable = true;                    // 점프 가능하도록 설정      
     }
 
     // 반복문          
@@ -28,10 +32,13 @@ public class AccControl : MonoBehaviour {
         vertical = Input.GetAxis("Vertical");
 
         // 방향키 관련 동작        
-        if (Input.GetKey(KeyCode.W))        transform.Translate(0, 0, runSpeed * Mathf.Log10((float)sp_));  // 전진   
-        else if (Input.GetKey(KeyCode.S))   transform.Translate(0, 0, -runSpeed);                           // 후진   
-        if (Input.GetKey(KeyCode.A))        transform.Translate(-runSpeed, 0, 0);                           // 좌측   
-        else if (Input.GetKey(KeyCode.D))   transform.Translate(runSpeed, 0, 0);                            // 우측   
+        if (Input.GetKey(KeyCode.W))        transform.Translate(0, 0, runSpeed * Mathf.Log10((float)moveLevel));   // 전진   
+        else if (Input.GetKey(KeyCode.S))   transform.Translate(0, 0, -runSpeed * Mathf.Log10((float)_MIN_VALUE)); // 후진   
+        if (Input.GetKey(KeyCode.A))        transform.Translate(-runSpeed * Mathf.Log10((float)_MIN_VALUE), 0, 0); // 좌측   
+        else if (Input.GetKey(KeyCode.D))   transform.Translate(runSpeed * Mathf.Log10((float)_MIN_VALUE), 0, 0);  // 우측   
+
+        if (Input.GetKeyDown(KeyCode.Space) && isJumpEnable) 
+            rigidbody.AddForce(Vector3.up * jumpPower, ForceMode.Impulse);
         
     }
 
@@ -40,17 +47,17 @@ public class AccControl : MonoBehaviour {
         while (true) {
 
             // 상태 표시            
-            Debug.Log("SP : " + sp_);
+            Debug.Log("SP : " + moveLevel);
 
             // 전진 중일 경우 속도를 위한 값을 조금씩 증가함.  
-            if (Input.GetKey(KeyCode.W) && sp_ < _MAX_VALUE + 1) {
-                sp_++;
-                yield return new WaitForSeconds(0.2f);
+            if (Input.GetKey(KeyCode.W) && moveLevel < _MAX_VALUE + 1) {
+                moveLevel++;
+                yield return new WaitForSeconds(0.2f - (moveLevel * 0.003f));
             }
 
             // 전진 중이 아닐 경우 속도를 위한 값을 감소함.   
-            else if (sp_ > _MIN_VALUE) {
-                sp_--;
+            else if (!Input.GetKey(KeyCode.W) && moveLevel > _MIN_VALUE) {
+                moveLevel -= 2;
                 yield return null;
             }
 
@@ -58,5 +65,17 @@ public class AccControl : MonoBehaviour {
 
             yield return null;
         }
+    }
+
+
+    // Ground 물체와 충돌할 경우 점프 활성화     
+    private void OnCollisionEnter(Collision collision) {
+        if (collision.gameObject.CompareTag("Ground") || collision.gameObject.CompareTag("MoveGround"))
+            isJumpEnable = true;
+    }
+
+    private void OnCollisionExit(Collision collision) {
+        if (collision.gameObject.CompareTag("Ground") || collision.gameObject.CompareTag("MoveGround"))
+            isJumpEnable = false;
     }
 }
