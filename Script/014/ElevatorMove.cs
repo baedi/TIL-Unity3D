038b7130ad3ebbfe04f4683262a340681c2a4c99
Daeil Bae <47047263+baedi@users.noreply.github.com>
2019-12-26 23:11:54 +0900

    Unity Study (2019-12-26)
    * ElevatorMove.cs 수정사항
    
    - 엘리베이터 버튼 클릭 시 발생하는 작동 방식(로직) 변경.
    - 엘리베이터가 부드럽게 움직이도록 수정함.

--- Script/014/ElevatorMove.cs
+++ Script/014/ElevatorMove.cs
@@ -7,10 +7,17 @@ public class ElevatorMove : MonoBehaviour {
     // 변수           
     private float locationLow;        // 낮은 y축 위치 (초기)  
     private float locationHigh;       // 높은 y축 위치         
-    private float maxHeight = 10.0f;  // 최대 높이             
-    private float moveSpeed = 0.1f;   // 속도                  
+    private float maxHeight = 50.0f;  // 최대 높이             
+
+    private float currentSpeed;
+    private float minSpeed = 0.01f;         // 최저 속도      
+    private float maxSpeed = 0.1f;          // 최대 속도      
+    private const float downSpeed = 0.95f;  // 속도 감소용    
+    private const float upSpeed = 1.005f;   // 속도 증가용    
 
     private bool isUpMode;            // 엘리베이터 상태 여부  
+    private bool enableOn;            // 엘리베이터 작동 여부  
+    
 
     // 초기화         
     void Start() {
@@ -20,24 +27,46 @@ public class ElevatorMove : MonoBehaviour {
         locationHigh = locationLow + maxHeight;
 
         // isUpMode를 false로 설정 (엘리베이터가 낮은 층에 있는 상태로 만드는 것)
-        isUpMode = false;
-
+        isUpMode = false;           
+        enableOn = false;           // 엘리베이터 비활성화   
+        currentSpeed = 0;           // 속도 초기화           
     }
 
     // 반복           
     void Update() {
-        
-        // 만약 올라기는 상태일 경우       
-        if(isUpMode && (transform.position.y < locationHigh)) 
-            transform.Translate(0, moveSpeed, 0);
-        
 
-        // 만약 내려가는 상태일 경우       
-        else if(!isUpMode && (transform.position.y > locationLow)) 
-            transform.Translate(0, -moveSpeed, 0);
-        
+        // 엘리베이터가 작동 상태일 경우     
+        if (enableOn) {
+
+            // 만약 올라기는 상태일 경우          
+            if (isUpMode && (transform.position.y < locationHigh)) {
+                currentSpeed = currentSpeed <= 0.1 ? currentSpeed * upSpeed : currentSpeed;
+                transform.Translate(0, currentSpeed, 0);
+            }
+
+            // 만약 내려가는 상태일 경우          
+            else if (!isUpMode && (transform.position.y > locationLow)) {
+                currentSpeed = currentSpeed <= 0.1 ? currentSpeed * upSpeed : currentSpeed;
+                transform.Translate(0, -currentSpeed, 0);
+            }
+
+            // 범위를 벗어날 경우 작동 상태 해제  
+            else enableOn = false;
+        }
+
+
+        // 범위를 벗어날 경우  (속도가 조금씩 줄어듬)        
+        else {
+            currentSpeed = currentSpeed * downSpeed;
+            transform.Translate(0, isUpMode ? currentSpeed : -currentSpeed, 0);
+        }
+
     }
 
     // 외부 호출용 (한번 발생하면 isUpMode 상태를 매번 바꿈)  
-    public void setUpDownMode() { isUpMode = !isUpMode; }
+    public void setUpDownMode() {
+        isUpMode = !isUpMode;       // 엘리베이터 올라가기/내려가기  
+        currentSpeed = minSpeed;    // 속도 초기화                   
+        enableOn = true;            // 엘리베이터 활성화             
+    }
 }
---
+++ Script/014/ElevatorMove_Old1.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElevatorMove_Old1 : MonoBehaviour {
+
+    // 변수           
+    private float locationLow;        // 낮은 y축 위치 (초기)  
+    private float locationHigh;       // 높은 y축 위치         
+    private float maxHeight = 10.0f;  // 최대 높이             
+    private float moveSpeed = 0.1f;   // 속도                  
+
+    private bool isUpMode;            // 엘리베이터 상태 여부  
+
+    // 초기화         
+    void Start() {
+
+        // 엘리베이터 시작, 끝 위치 설정                  
+        locationLow = transform.position.y;
+        locationHigh = locationLow + maxHeight;
+
+        // isUpMode를 false로 설정 (엘리베이터가 낮은 층에 있는 상태로 만드는 것)
+        isUpMode = false;
+
+    }
+
+    // 반복           
+    void Update() {
+        
+        // 만약 올라기는 상태일 경우       
+        if(isUpMode && (transform.position.y < locationHigh)) 
+            transform.Translate(0, moveSpeed, 0);
+        
+
+        // 만약 내려가는 상태일 경우       
+        else if(!isUpMode && (transform.position.y > locationLow)) 
+            transform.Translate(0, -moveSpeed, 0);
+        
+    }
+
+    // 외부 호출용 (한번 발생하면 isUpMode 상태를 매번 바꿈)  
+    public void setUpDownMode() { isUpMode = !isUpMode; }
+}
