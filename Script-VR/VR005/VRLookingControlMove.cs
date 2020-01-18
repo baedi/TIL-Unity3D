e79a7ec90276025d980a362ae5498b3c3dc3e8f0
Daeil Bae <47047263+baedi@users.noreply.github.com>
2020-01-18 23:45:25 +0900

    Unity Study (2020-01-18)
    - OVRCameraRig의 구성요소에 관한 이해 (CenterEyeAnchor의 용도)

---
+++ Script-VR/VR005/VRLookingControlMove.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VRLookingControlMove : MonoBehaviour {
+
+    // 변수        
+    private Transform centerEyeAnchorTransform;     // VR 중앙 시야 Transform    
+
+    public float moveSpeed = 2f;                    // 이동속도                  
+
+
+    // 초기화      
+    private void Start() {
+        centerEyeAnchorTransform = GameObject.Find("CenterEyeAnchor").GetComponent<Transform>();
+    }
+
+
+    // 루프        
+    private void Update() {
+        // 트리거 버튼을 누를 경우        
+        if (OVRInput.Get(OVRInput.Button.PrimaryIndexTrigger) || Input.GetKey(KeyCode.W))
+            transform.Translate(centerEyeAnchorTransform.forward * moveSpeed * Time.deltaTime);
+
+        
+    }
+}
---
+++ Script-VR/VR005/readmeVR005.md
@@ -0,0 +1,20 @@
+### VR003. VRLookingControlMove.cs
+
+
+#### 스크립트 설명
+	- Oculus GO의 컨트롤러 트리거 버튼을 누르면 플레이어가 바라본 화면으로 전진하는 스크립트
+
+
+
+#### 사용 방법
+	1. 빈 오브젝트를 만들어서 이름을 Player로 변경한다. 
+	2. Player 오브젝트에 OVRCameraRig 프리팹을 넣는다.
+	3. Player 오브젝트에 다음과 같은 컴포넌트를 추가한다. (스크립트 포함)
+		- VRLookingControlMove.cs
+		- Rigidbody (Freeze Rotate 설정 필요)
+		- Capsule Collider
+
+
+
+#### 배운 내용
+	- OVRCameraRig의 구성요소에 관한 이해 (CenterEyeAnchor의 용도)
