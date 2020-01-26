c9fce05b82c451eaf753e941e8c7796f5b915186
Daeil Bae <47047263+baedi@users.noreply.github.com>
2020-01-26 22:14:57 +0900

    Unity Study (2020-01-26)
    - Transform 관련, 일반과 로컬의 차이 이해

---
+++ Script-VR/VR007/VRRotateControl.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VRRotateControl : MonoBehaviour {
+
+    // 변수        
+    private Transform centerEyeAnchor;
+    private Transform ovrCameraRig;
+    //public float rotateSpeed = 6f;
+
+
+    // 초기화      
+    private void Awake() {
+        centerEyeAnchor = 
+            GameObject.Find("CenterEyeAnchor").GetComponent<Transform>();
+
+        ovrCameraRig =
+            GameObject.Find("OVRCameraRig").GetComponent<Transform>();
+    }
+
+
+    // 반복문      
+    private void Update() {
+
+        if(centerEyeAnchor.localRotation.x >= 0) {
+            ovrCameraRig.localPosition = new Vector3(ovrCameraRig.localPosition.x, -(centerEyeAnchor.localRotation.x / 2f), ovrCameraRig.localPosition.z);
+        }
+
+
+        /*
+        if(centerEyeAnchor.eulerAngles.x > 0 && centerEyeAnchor.eulerAngles.x <= 90) {
+            transform.rotation = Quaternion.Lerp(transform.rotation, centerEyeAnchor.rotation, rotateSpeed * Time.deltaTime);
+        }
+
+        else {
+            transform.rotation = Quaternion.Lerp(transform.rotation, new Quaternion(0, transform.rotation.y, transform.rotation.z, transform.rotation.w), rotateSpeed * Time.deltaTime);
+        }
+        */
+
+        //transform.rotation = Quaternion.Lerp(transform.rotation, centerEyeAnchor.rotation, rotateSpeed * Time.deltaTime);
+
+        //transform.eulerAngles = Vector3.Lerp(transform.eulerAngles, new Vector3(centerEyeAnchor.eulerAngles.x, transform.eulerAngles.y, transform.eulerAngles.z), rotateSpeed * Time.deltaTime);
+    }
+}
---
+++ Script-VR/VR007/readmeVR007.md
@@ -0,0 +1,14 @@
+### VR007. VRRotateControl.cs
+
+#### 스크립트 설명
+	- (Oculus GO 전용) HMD를 착용한 상태에서 고개를 숙인 정도에 따라 카메라 y축 위치가 조절되는 스크립트
+
+
+#### 사용 방법
+	1. 빈 오브젝트를 만들어서 이름을 Player로 변경한다. 
+	2. Player 오브젝트에 OVRCameraRig 프리팹을 넣는다.
+	3. Player 오브젝트에 VRRotateControl.cs 스크립트를 추가한다.
+
+
+#### 배운 내용
+	- Transform 관련, 일반과 로컬의 차이 이해
