09e68b1b958bf16858ef6d48f2b6fc987288a6aa
Daeil Bae <47047263+baedi@users.noreply.github.com>
2020-02-07 23:19:17 +0900

    Unity Study (2020-02-07)
    - 씬이 변경되어도 제거되지 않는 오브젝트 만드는 방법
    - System.Obsolve 네임스페이스 없이 게임오브젝트 활성화/비활성화 방법
    - 인덱서로 자식 오브젝트 찾는 방법

---
+++ Script-VR/VR012/PhotonInit2.cs
@@ -0,0 +1,118 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Photon.Pun;
+using Photon.Realtime;
+
+using Mono.Data.Sqlite;
+
+
+public class PhotonInit2 : MonoBehaviourPunCallbacks
+{
+
+    // Var              
+    public string gameVer = "1.0";
+    public string nickName = "default";
+
+    private SqliteConnection connect;
+    private SqliteCommand command;
+    private string dbName = @"\user.db";
+    private string dbPath;
+
+
+    // Initialize       
+    private void Awake() {
+
+        // DB Setting.      
+        if (Application.platform == RuntimePlatform.Android)
+            dbPath = Application.persistentDataPath + dbName;
+
+        else
+            dbPath = Application.dataPath + dbName;
+
+        connect = new SqliteConnection("Data Source=" + dbPath + ";Version=3;");
+        connect.Open();
+
+        command = new SqliteCommand("SELECT * FROM userdata;", connect);
+        SqliteDataReader reader = command.ExecuteReader();
+        if (reader.Read()) nickName = reader["nickname"].ToString();
+        reader.Close();
+        connect.Close();
+
+        PhotonNetwork.AutomaticallySyncScene = true;
+    }
+
+    private void Start() {
+        OnLogin();
+    }
+
+
+    // Login Network        
+    private void OnLogin() {
+        PhotonNetwork.GameVersion = gameVer;
+        PhotonNetwork.NickName = nickName;
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+
+    // Override Method.     
+    public override void OnConnectedToMaster() {
+        base.OnConnectedToMaster();
+
+        Debug.Log("Connect player : " + nickName);
+
+        // Join Room        
+
+        GameObject hostUserCheck = GameObject.Find("CreateRoom_");
+
+        // Host User.       
+        if(hostUserCheck != null) {
+            Debug.Log(hostUserCheck.name);
+            string hostname = hostUserCheck.transform.GetChild(0).name;
+            Debug.Log(hostname);
+            PhotonNetwork.CreateRoom(hostname, new RoomOptions { MaxPlayers = 4, IsVisible = true });
+            Destroy(hostUserCheck);
+            Debug.Log("Create Success!!!");
+        }
+
+        // Other User.      
+        else {
+            hostUserCheck = GameObject.Find("JoinRoom_");
+            string roomname = hostUserCheck.transform.GetChild(0).name;
+            PhotonNetwork.JoinRoom(roomname);
+            Destroy(hostUserCheck);
+        }
+    }
+
+
+    // Failed Join Room (Create Room)   
+    public override void OnJoinRandomFailed(short returnCode, string message) {
+        SceneManager.LoadScene("LobbyScene");
+    }
+
+    // Join Room (My or Other Player's Room)  
+    public override void OnJoinedRoom()
+    {
+        base.OnJoinedRoom();
+
+        Debug.Log("Join Room");
+        GameObject tempObj = PhotonNetwork.Instantiate("Player", GameObject.Find("SpawnPoint").transform.position, Quaternion.identity);
+        tempObj.GetComponentInChildren<SkinnedMeshRenderer>().enabled = false;
+        GameObject.Find("OVRCameraRig").transform.parent = tempObj.transform;
+        GameObject.Find("OVRCameraRig").transform.localPosition = new Vector3(0, 0.8f, 0);
+    }
+
+
+    // Another player entered room function.    
+    public override void OnPlayerEnteredRoom(Player newPlayer) {
+        base.OnPlayerEnteredRoom(newPlayer);
+        Debug.Log("!");
+    }
+
+    // Another player leave room function.      
+    public override void OnPlayerLeftRoom(Player otherPlayer) {
+        base.OnPlayerLeftRoom(otherPlayer);
+    }
+
+}
--- Script-VR/VR012/PhotonManager.cs
+++ Script-VR/VR012/PhotonManager.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 using Photon.Pun;
 using Photon.Realtime;
 using ExitGames.Client.Photon;
@@ -12,8 +13,22 @@ public class PhotonManager : MonoBehaviourPunCallbacks {
     // Var              
     public GameObject[] roomGameObject = new GameObject[4];
 
+    private GameObject createRoomUI;
+    private GameObject keyboardUI;
+
+
     // Initialize       
     private void Awake() {
+
+        // Object Enable Settings.  
+        createRoomUI = GameObject.Find("CreateRoomUI");
+        createRoomUI.SetActive(false);
+
+        keyboardUI = GameObject.Find("KeyboardCanvas");
+        keyboardUI.SetActive(false);
+
+
+        // Photon Network Settings. 
         PhotonNetwork.AutomaticallySyncScene = true;
         PhotonNetwork.GameVersion = "1.0";
         PhotonNetwork.NickName = "Default";
@@ -41,15 +56,16 @@ public class PhotonManager : MonoBehaviourPunCallbacks {
     }
 
 
+    // (Callback) Room Update       
     public override void OnRoomListUpdate(List<RoomInfo> roomList) {
         base.OnRoomListUpdate(roomList);
 
         Debug.Log("Room Count : " + roomList.Count);
 
-        for(int count = 0; count < roomGameObject.Length; count++) {
+        for (int count = 0; count < roomGameObject.Length; count++) {
 
             // Room Avaliable.      
-            if(count < roomList.Count) {
+            if (count < roomList.Count) {
                 var roomItem = roomList[count];
                 PhotonRoomInfo roominfo = roomGameObject[count].GetComponent<PhotonRoomInfo>();
                 roominfo.RoomName = roomItem.Name;
@@ -59,7 +75,7 @@ public class PhotonManager : MonoBehaviourPunCallbacks {
                 if (roominfo.RoomPlayer != 0)
                     roomGameObject[count].GetComponent<Button>().interactable = true;
 
-                else 
+                else
                     roomGameObject[count].GetComponent<Button>().interactable = false;
 
                 roominfo.RoomInfoTextChange();
@@ -71,8 +87,81 @@ public class PhotonManager : MonoBehaviourPunCallbacks {
             }
 
         }
-
-
     }
 
+
+
+    /*******************************/
+    /*** Button Click Event List ***/
+    /*******************************/
+
+    // Room Button Click Event          
+    public void OnRoomButtonClick() {
+
+        string roomname = GameObject.Find("CreateRoomInputButton").GetComponentInChildren<Text>().text;
+        if (roomname.Length <= 0) return;
+
+        // Get Room Info (Name)     
+        GameObject temp = new GameObject();
+        GameObject temp2 = new GameObject();
+        temp.name = roomname;
+        temp2.name = "CreateRoom_";
+        temp.transform.parent = temp2.transform;
+
+        DontDestroyOnLoad(temp);
+        DontDestroyOnLoad(temp2);
+
+        PhotonNetwork.LeaveLobby();
+        PhotonNetwork.Disconnect();
+        SceneManager.LoadScene("GameScene2");
+    }
+
+
+    // Join Room Button Click Event         
+    public void OnJoinRoomButtonClick(GameObject clickObject) {
+
+        GameObject temp = new GameObject();
+        GameObject temp2 = new GameObject();
+        temp.name = clickObject.GetComponent<PhotonRoomInfo>().RoomName;
+        temp2.name = "JoinRoom_";
+        temp.transform.parent = temp2.transform;
+
+        DontDestroyOnLoad(temp);
+        DontDestroyOnLoad(temp2);
+
+        PhotonNetwork.LeaveLobby();
+        PhotonNetwork.Disconnect();
+        SceneManager.LoadScene("GameScene2");
+    }
+
+
+    // Room Create Button Click Event   
+    public void OnCreateRoomButtonClick() {
+
+        // Visible CreateRoom UI Interface  
+        if (createRoomUI.activeInHierarchy) {
+            createRoomUI.SetActive(false);
+
+            if (keyboardUI.activeInHierarchy)
+                keyboardUI.SetActive(false);
+        }
+        else createRoomUI.SetActive(true);
+    }
+
+
+    // Keyboard Active Event.           
+    public void OnKeyboardActiveEvent(GameObject target) {
+
+        // IF Keyboard state is already active  
+        if (keyboardUI.activeInHierarchy) {
+            keyboardUI.SetActive(false);
+            return;
+        }
+
+        // Keyboard state is not active.        
+        keyboardUI.SetActive(true);
+
+        for (int count = 0; count < keyboardUI.GetComponentsInChildren<ButtonKey>().Length; count++)
+            keyboardUI.GetComponentsInChildren<ButtonKey>()[count].setTargetTextObject(target.GetComponentInChildren<Text>().gameObject);
+    }
 }
