87ddee745dab14d299585f896e72b78d58ef4202
Daeil Bae <47047263+baedi@users.noreply.github.com>
2020-03-01 11:06:34 +0900

    Unity Study (2020-03-01)

---
+++ Script/040/SceneLoadManager.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+// 로딩 씬 전용 스크립트         
+/**
+ * 파괴 불가능한 오브젝트 전용
+ * NextSceneInfo로부터 씬 정보를 가져온 뒤 로드함.
+ **/
+public class SceneLoadManager : MonoBehaviour {
+
+    // 변수               
+    public GameObject loadCanvas;
+
+    private Text loadingText;
+    private Slider loadingBar;
+    private AsyncOperation async;
+    private bool isComplete = false;
+
+
+    // 초기화              
+    private void Start() {
+
+        /** 컴포넌트 가져옴 **/
+        loadingText = loadCanvas.GetComponentInChildren<Text>();
+        loadingBar = loadCanvas.GetComponentInChildren<Slider>();
+
+        /** 불러올 씬 이름을 가져온 뒤 로드함 **/
+        string loadSceneName = GameObject.Find("SceneManager").GetComponent<NextSceneInfo>().NextSceneName;
+        async = SceneManager.LoadSceneAsync(loadSceneName);
+
+        /** 로딩이 끝나면 자동으로 불러오지 않도록 설정함 **/
+        async.allowSceneActivation = false;
+
+        StartCoroutine(LoadingProcessManager());
+        StartCoroutine(ProgressLerp());
+    }
+
+    // (코루틴) 불러올 씬의 로드를 확인하기 위한 함수         
+    private IEnumerator LoadingProcessManager() {
+
+        while (true) {
+            loadingText.text = $"로딩 중... ({(int)(async.progress * 100)}%)";
+
+            /** 90%에 도달할 경우 일정 시간 지연 후 반복문 탈출 **/
+            if(async.progress >= 0.9f) {
+                yield return new WaitForSeconds(1.5f);
+                break;
+            }
+
+            yield return new WaitForSeconds(0.1f);
+        }
+
+        isComplete = true;
+        loadingText.text = "불러오는 중...";
+
+        yield return new WaitForSeconds(1f);
+        async.allowSceneActivation = true;
+
+        yield return null;
+    }
+
+
+    // (코루틴) 로딩 바의 흐름을 부드럽게 연출하기 위한 함수  
+    private IEnumerator ProgressLerp() {
+
+        while (!async.isDone) {
+
+            /** 부드러운 로딩 바 연출 **/
+            loadingBar.value = Mathf.Lerp(loadingBar.value, isComplete ? 1f : async.progress, 
+                                            4f * Time.deltaTime);
+
+            if (loadingBar.value == 1f) break;
+
+            yield return null;
+        }
+
+        yield return null;
+    }
+}
--- Script/040/readme040.md
+++ Script/040/readme040.md
@@ -3,6 +3,7 @@
 
 #### 스크립트 설명 
 	- NextSceneInfo.cs : 로딩 씬을 불러오면서 다음 불러올 씬을 관리할 수 있는 스크립트
+	- SceneLoadManager.cs : SceneManager 오브젝트로부터 로드할 씬 이름을 가져와서 로딩을 수행하는 스크립트
 
 
 #### 사용 방법 
@@ -12,8 +13,14 @@
 		- MainScene
 
 	2. StartScene 씬을 불러온다.
+
 	3. 빈 오브젝트를 생성하고 <NextSceneInfo> 스크립트를 적용한다.
+		- 이름은 SceneManager로 변경
+
+	4. LoadingScene 씬을 불러온다.
+
+	5. 빈 오브젝트를 생성하고 <SceneManager> 스크립트를 적용한다.
 
 
-#### 배운 내용 
+
 
