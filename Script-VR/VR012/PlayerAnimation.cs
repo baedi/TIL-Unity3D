6ed877f7dcea1d51a9626223ef17a74a6e0fdc96
Daeil Bae <47047263+baedi@users.noreply.github.com>
2020-03-16 18:56:38 +0900

    Unity Study (2020-03-16)

---
+++ Script-VR/VR012/PlayerAnimation.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class PlayerAnimation : MonoBehaviourPunCallbacks {
+
+    // 변수                
+    private Animator animator;
+    public float detect_touchAxis = 0.5f;
+
+    // 초기화              
+    private void Start() {
+        animator = GetComponent<Animator>();
+    }
+
+    // 루프                
+    private void Update() {
+
+        /** 자신이 컨트롤하는 오브젝트가 아니면 리턴 **/
+        if (!photonView.IsMine) return;
+
+        /** VR 컨트롤러 터치 정보를 가져옴 **/
+        Vector2 touchVector = OVRInput.Get(OVRInput.Axis2D.PrimaryTouchpad, OVRInput.Controller.RTrackedRemote);
+
+
+        /** 앞으로 이동 **/
+        if (Input.GetKey(KeyCode.W) || touchVector.y >= detect_touchAxis) {
+            animator.SetBool("isWalking", true);
+            animator.SetBool("isForward", true);
+            animator.SetBool("isBackward", false);
+        }
+
+        /** 뒤로 이동 **/
+        else if (Input.GetKey(KeyCode.S) || touchVector.y <= -detect_touchAxis) {
+            animator.SetBool("isWalking", true);
+            animator.SetBool("isForward", false);
+            animator.SetBool("isBackward", true);
+        }
+
+        else animator.SetBool("isWalking", false);
+
+    }
+}
---
+++ Script-VR/VR012/PlayerMove2.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+// 플레이어 이동 컨트롤 스크립트         
+/**
+ * Player 오브젝트 전용 스크립트
+ **/
+
+public class PlayerMove2 : MonoBehaviourPunCallbacks {
+
+    // 변수               
+    public float movingSpeed = 1.5f;        /** 이동 속도 **/
+    public float rotatingSpeed = 80f;       /** 회전 속도 **/
+    public float detect_touchAxis = 0.5f;   /** VR 컨트롤러 버튼 감지용 **/
+
+
+    // 루프               
+    private void Update() {
+
+        /** 자신이 컨트롤하는 오브젝트가 아니면 리턴 **/
+        if (!photonView.IsMine) return;
+
+        /** VR 컨트롤러 터치 정보를 가져옴 **/
+        Vector2 touchVector = OVRInput.Get(OVRInput.Axis2D.PrimaryTouchpad, OVRInput.Controller.RTrackedRemote);
+
+
+        /** 앞으로 이동 **/
+        if (Input.GetKey(KeyCode.W) || touchVector.y >= detect_touchAxis)
+            transform.Translate(Vector3.forward * movingSpeed * Time.deltaTime);
+
+        /** 뒤로 이동 **/
+        else if (Input.GetKey(KeyCode.S) || touchVector.y <= -detect_touchAxis)
+            transform.Translate(Vector3.back * movingSpeed * Time.deltaTime);
+
+        /** 좌측으로 회전 **/
+        if (Input.GetKey(KeyCode.A) || touchVector.x <= -detect_touchAxis)
+            transform.Rotate(Vector3.down * rotatingSpeed * Time.deltaTime);
+
+        /** 우측으로 회전 **/
+        else if (Input.GetKey(KeyCode.D) || touchVector.x >= detect_touchAxis)
+            transform.Rotate(Vector3.up * rotatingSpeed * Time.deltaTime);
+    }
+}
