ee0d1d4e3d086db6f0ebe9916c7348c9acde094b
Daeil Bae <47047263+baedi@users.noreply.github.com>
2020-03-17 22:42:46 +0900

    Unity Study (2020-03-17)
     - (아이템)물조리개 제어 스크립트 추가
     - 이펙트 OFF 스크립트 추가

---
+++ Script-VR/VR014/ParticleStopInitialize.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**
+ * 파티클이 적용된 오브젝트 전용 스크립트.
+ * 처음 시작 시 파티클이 재생되기 싫을 경우 사용됨.
+ **/
+public class ParticleStopInitialize : MonoBehaviour {
+
+    // 초기화                  
+    private void Start() {
+        GetComponentInChildren<ParticleSystem>().Stop();
+    }
+}
---
+++ Script-VR/VR014/WaterCan.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterCan : ItemManager {
+
+    // 변수               
+    private bool isWaterOn = false;     /** 물 이펙트 활성화 여부 **/
+    private float lerpPoint = 4f;
+
+    public GameObject rHandAnchor;      /** 우측 핸드 컨트롤러 Transform **/
+    public GameObject effectObj;
+    public GameObject effectPosition;    
+
+    // 반복문             
+    private void Update() {
+
+        /** 위치 고정, 이펙트 선형 보간 이용 **/
+        transform.localPosition = Vector3.Lerp(transform.localPosition, new Vector3(0, 0, 0), lerpPoint * Time.deltaTime);
+        effectObj.transform.position = effectPosition.transform.position;
+        effectObj.transform.rotation = Quaternion.Lerp(effectObj.transform.rotation, effectPosition.transform.rotation, 0.5f * Time.deltaTime) ;
+
+        /** 기울기 여부 확인 **/
+        if ((rHandAnchor.transform.localEulerAngles.x >= 45 && rHandAnchor.transform.localEulerAngles.x <= 80) && !isWaterOn)
+            WaterEffect(true);
+
+        else if(!(rHandAnchor.transform.localEulerAngles.x >= 45 && rHandAnchor.transform.localEulerAngles.x <= 80) && isWaterOn) WaterEffect(false);
+
+        /** 아이템 장착 해제 **/
+        if(OVRInput.GetDown(OVRInput.Button.Back) || Input.GetKeyDown(KeyCode.R)) {
+            OnBackButtonClick();
+        }
+        
+    }
+
+    // 뒤로가기 버튼 클릭   
+    public override void OnBackButtonClick() {
+        base.OnBackButtonClick();
+        Debug.Log("Water Can!");
+    }
+
+    // 물 이펙트 발생 여부  
+    private void WaterEffect(bool waterOn) {
+        isWaterOn = waterOn;
+
+        if (isWaterOn){
+            effectObj.GetComponentInChildren<ParticleSystem>().Play();
+        }
+
+        else{
+            effectObj.GetComponentInChildren<ParticleSystem>().Stop();
+        }
+    }
+}
