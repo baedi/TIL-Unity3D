14eace500d2a2162e99e6a378ddf451e90a93ff1
Daeil Bae <47047263+baedi@users.noreply.github.com>
2020-03-25 22:39:09 +0900

    Unity Study (2020-03-25)
    - BigShovel.cs 수정 (흙 파기 관련 코드 추가)

--- Script-VR/VR014/BigShovel.cs
+++ Script-VR/VR014/BigShovel.cs
@@ -5,9 +5,23 @@ using UnityEngine;
 public class BigShovel : ItemManager {
 
     // 변수               
-    private bool isSoilEnable;  /** 흙 활성화 여부 **/
+    private bool isSoilEnable;          /** 흙 활성화 여부 **/
     private float digDistance = 0.36f;
     private float lerpPoint = 4f;
+
+    private GameObject shovel_normal_model;
+    private GameObject shovel_lomp_model;
+
+
+    // 초기화              
+    private void Start() {
+        /** 아이템을 최초 장착했을 시 동작함. **/
+        Debug.Log("BigShovel Start( )");
+        shovel_normal_model = transform.GetChild(0).gameObject;
+        shovel_lomp_model = transform.GetChild(1).gameObject;
+        shovel_normal_model.SetActive(true);
+        shovel_lomp_model.SetActive(false);
+    }
 
     // 반복문             
     private void Update() {
@@ -16,6 +30,16 @@ public class BigShovel : ItemManager {
         if (OVRInput.Get(OVRInput.Button.PrimaryIndexTrigger) || Input.GetMouseButton(0)) {
             transform.localPosition = Vector3.Lerp(transform.localPosition, Vector3.forward * digDistance, lerpPoint * 2 * Time.deltaTime);
         }
+
+        else if(OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger) || Input.GetMouseButtonDown(0)) {
+            /** 삽에 흙이 존재할 경우 뿌리기 **/
+            if (isSoilEnable) {
+                isSoilEnable = false;
+                shovel_normal_model.SetActive(true);
+                shovel_lomp_model.SetActive(false);
+            }
+        }
+
 
         /** 삽 원래대로 **/
         else {
@@ -38,16 +62,14 @@ public class BigShovel : ItemManager {
     // 트리거 감지           
     private void OnTriggerEnter(Collider other) {
 
-        if (!other.gameObject.CompareTag("trig_Soil")) return;
+        if (!other.gameObject.CompareTag("trig_dig")) return;
 
-        /** 흙이 없는 경우 **/
-        if (isSoilEnable) {
-
-        }
-
-        /** 흙이 있는 경우 **/
-        else {
-
+        /** 흙이 있으면서 버튼을 누른 상태 **/
+        if(OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger) && !isSoilEnable){
+            isSoilEnable = true;
+            shovel_normal_model.SetActive(false);
+            shovel_lomp_model.SetActive(true);
         }
     }
+
 }
