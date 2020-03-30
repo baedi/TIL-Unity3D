21b4fbb8e05936d4214c3a207c5737c8a06e7ddf
Daeil Bae <47047263+baedi@users.noreply.github.com>
2020-03-30 21:50:27 +0900

    Unity Study (2020-03-30)

--- Script-VR/VR014/Potato.cs
+++ Script-VR/VR014/Potato.cs
@@ -11,6 +11,8 @@ public class Potato : ItemManager {
 
     // 변수               
     private float lerpPoint = 4f;
+    private float raycastDistance = 1.5f;
+    private RaycastHit raycastHit;
 
     // 초기화             
     private void Start() {
@@ -21,8 +23,19 @@ public class Potato : ItemManager {
     private void Update() {
 
         /** Raycast 활성화 **/
-            /** plant 콜라이더 감지 시 **/
-            /** 만약 트리거 버튼 누를 시 심고, 아이템은 장착 해제 상태로 변경 **/ 
+        Debug.DrawRay(transform.position, transform.forward * raycastDistance, Color.green, 0.5f);
+
+        /** 감지 확인 **/
+        if(Physics.Raycast(transform.position, transform.forward, out raycastHit, raycastDistance) && this.enabled) {
+
+            /** plant 콜라이더 감지했을 때 버튼도 같이 누를 경우 **/
+            if(raycastHit.collider.gameObject.CompareTag("col_plant") && 
+                (OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger) || 
+                Input.GetKeyDown(KeyCode.E))) {
+
+            }
+        }
+
 
         /** 아이템이 항상 손에서 따라다님 **/
         transform.localPosition = Vector3.Lerp(transform.localPosition, new Vector3(0, 0, 0), lerpPoint * Time.deltaTime);
