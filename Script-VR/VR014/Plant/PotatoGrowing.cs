941e75707a655dfdecfbc3a1b21d1d77e32107a1
Daeil Bae <47047263+baedi@users.noreply.github.com>
2020-04-01 23:45:49 +0900

    Unity Study (2020-04-01)

---
+++ Script-VR/VR014/Plant/PotatoGrowing.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PotatoGrowing : MonoBehaviour {
+
+    //  변수                  
+    public GameObject[] potatoPrefab;
+
+    private int level;
+
+
+    //  초기화                 
+    private void Start() {
+        level = 0;
+        SetLevel(level);
+    }
+
+
+    // 레벨 상승               
+    public void GrowLevelUp() {
+
+        if(level < potatoPrefab.Length - 1) {
+            level++;
+            SetLevel(level);
+        }
+
+    }
+
+
+    // 레벨 설정               
+    private void SetLevel(int input_level) {
+
+        if(level < potatoPrefab.Length) {
+            for(int index = 0; index < potatoPrefab.Length; index++) {
+                if (input_level == index)
+                    potatoPrefab[index].SetActive(true);
+
+                else potatoPrefab[index].SetActive(false);
+            }
+        }
+
+    }
+
+}
