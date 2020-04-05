a8ace173d59d89d710b147e35ef5bdc163328c01
Daeil Bae <47047263+baedi@users.noreply.github.com>
2020-04-05 20:35:26 +0900

    Unity Study (2020-04-05)
     * Add C# Scripts
     - GuideContentBox.cs
     - GuideManager.cs

---
+++ Script/042/GuideContentBox.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GuideContentBox : MonoBehaviour {
+    
+    // 프로퍼티         
+    public string TextBlock { get; private set; }
+    public Texture ImageBlock { get; private set; }
+    public ContentType BehaviourMode { get; private set; }
+
+    public enum ContentType
+    {
+        Normal = 0, /** 일반 모드 **/
+        Mission,    /** 미션 모드 **/
+        Switching,  /** 검은 화면 전환 모드 **/
+        Complete    /** 가이드 완료 모드 **/
+    }
+
+
+    // 생성자           
+    /**
+     * 인자1 : UI에 보일 텍스트
+     * 인자2 : UI에 보일 이미지
+     * 인자3 : 도구를 사용해야 하는 행동 모드인지 여부
+     **/
+    public GuideContentBox(string text, Texture image = null, ContentType behaviourType = ContentType.Normal) {
+        TextBlock = text;
+        ImageBlock = image;
+        BehaviourMode = behaviourType;
+    }
+}
---
+++ Script/042/GuideManager.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GuideManager : MonoBehaviour {
+
+    //  변수                   
+    public GuideContentBox[] content;   /** 콘텐츠 박스 **/ 
+    private int page;                   /** 페이지 **/
+
+}
