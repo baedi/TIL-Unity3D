7b023070d1bb74b40c689df51e0e9455ffb64814
Daeil Bae <47047263+baedi@users.noreply.github.com>
2020-04-11 23:53:21 +0900

    Unity Study (2020-04-11)
     * Add C# Script (041)
     - SoundManageModule.cs

---
+++ Script/041/SoundManageModule.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/** 사운드 관리 모듈
+ *  - 유형 : 부모 클래스
+ *  - 설명 : 원하는 사운드를 넣고 외부 스크립트를 이용하여 조절하기 위한 스크립트
+ **/
+public class SoundManageModule : MonoBehaviour {
+
+    // 변수                
+    public GameObject mainManager;  /** 메인 매니저 오브젝트 **/
+    public AudioClip sound;         /** 사운드 파일 **/
+
+    // 초기화              
+    private void Start() {
+
+        /** 오디오소스 컴포넌트 생성 **/
+        if (GetComponent<AudioSource>() == null)
+            this.gameObject.AddComponent<AudioSource>();
+
+        /** 사운드 볼륨 설정 **/
+        float[] volumes = mainManager.GetComponent<DatabaseManager>().GetVolumeSettings();
+        if (volumes[1] == -0.1f) volumes[1] = 0.8f;
+
+        /** 사운드 설정 **/
+        GetComponent<AudioSource>().volume = volumes[1];
+    }
+
+
+}
