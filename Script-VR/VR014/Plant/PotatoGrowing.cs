c5422ca12822743076b8f85bd4faea22d16226bc
Daeil Bae <47047263+baedi@users.noreply.github.com>
2020-04-21 23:48:22 +0900

    Unity Study (2020-04-21)
    Update C# Script (VR014)
    - PotatoGrowing.cs

--- Script-VR/VR014/Plant/PotatoGrowing.cs
+++ Script-VR/VR014/Plant/PotatoGrowing.cs
@@ -7,6 +7,7 @@ public class PotatoGrowing : MonoBehaviour {
     //  변수                  
     private int maxLevel;
     private int level;
+    private float yPosUp = 0.15f;
 
 
     //  초기화                 
@@ -16,18 +17,34 @@ public class PotatoGrowing : MonoBehaviour {
         level = 0;
 
         SetLevel(level);
-        StartCoroutine(TestGrow());
     }
 
 
     // 레벨 상승               
     public void GrowLevelUp() {
 
+        if(level == 0) {
+            this.transform.position += (Vector3.up * yPosUp);
+        }
+
         if(level < maxLevel - 1) {
             level++;
             SetLevel(level);
         }
 
+    }
+
+
+    // 레벨 하락                
+    public void GrowLevelDown() {
+        if(level == 1) {
+            this.transform.position -= (Vector3.up * yPosUp);
+        }
+
+        if(level < maxLevel - 1) {
+            level--;
+            SetLevel(level);
+        }
     }
 
 
@@ -47,15 +64,4 @@ public class PotatoGrowing : MonoBehaviour {
 
     }
 
-
-    /** 테스트용 **/
-    private IEnumerator TestGrow() {
-        for(int index = 0; index < 4; index++) {
-            yield return new WaitForSeconds(5);
-            GrowLevelUp();
-            yield return null;
-        }
-
-        yield return null;
-    }
 }
