71ef0903fa6aa068cbe5fb24147d2192f564f885
Daeil Bae <47047263+baedi@users.noreply.github.com>
2020-05-08 23:26:25 +0900

    Unity Study (2020-05-08)

---
+++ Script-VR/VR014/Plant/HarvestPlant.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+
+/**
+ * HarvestPlants.cs
+ * 
+ * 모든 재배 식물 프리팹을 위한 부모 클래스.
+ * 재배 식물 프리팹이 가진 기본적인 요소(식물 성장, 성장 설정)을 구성함.
+ * 
+ * 실제 작물 프리팹에서 이 기능 적용하려고 할 경우 이 클래스를 상속받는 자식 클래스(컴포넌트)를 넣어주어야 함.
+**/
+public class HarvestPlant : MonoBehaviour {
+
+    /** 프로퍼티 **/
+    public int MaxLevel { get; protected set; }         // 최대 성장 레벨     
+    public int CurLevel { get; set; } = 0;              // 현재 성장 레벨     
+    public bool IsSavingEnable { get; set; } = false;   // DB 저장가능 여부   
+
+
+    /** 초기화 **/
+    [System.Obsolete]
+    private void Start() {
+        
+        MaxLevel        = transform.GetChildCount();   // 해당 스크립트를 적용한 식물 프리팹의 자식 오브젝트 개수에 따라 최대 레벨 결정.
+
+        SetLevel(CurLevel);
+    }
+
+
+    /** 식물 성장 레벨 올리기 **/
+    public virtual void GrowLevelUp() {
+
+        // 최대 레벨 -1 보다 현재레벨이 낮아야 성장 가능.  
+        if (CurLevel < MaxLevel - 1) {
+            CurLevel++;
+            SetLevel(CurLevel);
+        }
+    }
+
+
+    /** 식물 성장 레벨 임의 설정하기 **/
+    public virtual void SetLevel(int input_level) {
+
+        // 현재 레벨이 최대 레벨보다 작을 경우 첫번째 인자의 인덱스 값에 해당하는 식물 오브젝트만 보여준다.
+        if (CurLevel < MaxLevel) {
+            for (int index = 0; index < MaxLevel; index++) {
+
+                if (input_level == index)
+                    transform.GetChild(index).gameObject.SetActive(true);
+
+                else transform.GetChild(index).gameObject.SetActive(false);
+
+            }
+        }
+    }
+    
+}
--- Script-VR/VR014/Plant/PotatoGrowing.cs
+++ Script-VR/VR014/Plant/PotatoGrowing.cs
@@ -1,67 +1,50 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class PotatoGrowing : MonoBehaviour {
+/**
+ * PotatoGrowing.cs (HarvestPalnt 상속 받음)
+ * 
+ * 식물 감자 전용 프리팹에 이 스크립트를 적용함.
+**/
+public class PotatoGrowing : HarvestPlant {
 
-    //  변수                  
-    private int maxLevel;
-    private int level;
-    private float yPosUp = 0.15f;
-
-
-    //  초기화                 
-    [System.Obsolete]
-    private void Start() {
-        maxLevel = transform.GetChildCount();
-        level = 0;
-
-        SetLevel(level);
-    }
+    /** 변수 **/
+    public GameObject bugPrefab;
 
 
-    // 레벨 상승               
-    public void GrowLevelUp() {
+    /** 식물 성장 레벨 올리기 (오버라이드) **/
+    public override void GrowLevelUp() {
+        base.GrowLevelUp();
 
-        if(level == 0) {
-            this.transform.position += (Vector3.up * yPosUp);
+        if(CurLevel == 0) {
+            this.transform.position += (Vector3.up * 0.15f);
         }
 
-        if(level < maxLevel - 1) {
-            level++;
-            SetLevel(level);
-        }
-
-    }
-
-
-    // 레벨 하락                
-    public void GrowLevelDown() {
-        if(level == 1) {
-            this.transform.position -= (Vector3.up * yPosUp);
-        }
-
-        if(level < maxLevel - 1) {
-            level--;
-            SetLevel(level);
+        else if(CurLevel == 2) {
+            GetComponent<Collider>().enabled = true;
         }
     }
 
 
-    // 레벨 설정               
-    private void SetLevel(int input_level) {
+    /** 해충 생성(1레벨 전용) **/
+    public void CreateBugs_LV1() {
+        /** 생성 지점 초기화 **/
+        Vector3[] bugPosition = new Vector3[3];
+        Vector3[] bugRotation = new Vector3[3];
 
-        if(level < maxLevel) {
-            for(int index = 0; index < maxLevel; index++) {
+        bugPosition[0] =  new Vector3(-0.00486f, 0.0924f, -0.0023f);
+        bugRotation[0] =  new Vector3(15f, 0f, 0f);
+        bugPosition[1] =  new Vector3(-0.006f, 0.1061f, -0.0094f);
+        bugRotation[1] =  new Vector3(15f, 90f, 0f);
+        bugPosition[2] =  new Vector3(0.0119f, 0.0977f, -0.0036f);
+        bugRotation[2] =  new Vector3(8f, 180f, 0f);
 
-                if (input_level == index)
-                    transform.GetChild(index).gameObject.SetActive(true);
-
-                else transform.GetChild(index).gameObject.SetActive(false);
-
-            }
+        int rand = Random.Range(1, 4);
+        for(int count = 0; count < rand; count++) {
+            GameObject obj = Instantiate(bugPrefab);
+            obj.transform.parent = this.transform;
+            obj.transform.localPosition = bugPosition[count];
+            obj.transform.localEulerAngles = bugRotation[count];
         }
-
     }
 
 }
